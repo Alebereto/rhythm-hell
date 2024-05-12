f5a3bceb7c9eb3d408e31a8ad4b7fee60f3d12ba
Albert Khayat <1albertkhayat1@gmail.com>
2024-05-12 16:39:37 +0300

    Rename world to main

---
+++ scripts/main.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+var interface : XRInterface
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interface = XRServer.find_interface("OpenXR")
+	if interface and interface.is_initialized():
+		print("OpenXR instantiated successfully.")
+		var vp : Viewport = get_viewport()
+		
+		# Enable xr on viewport
+		vp.use_xr = true
+		
+		# Make sure v-sync is off, v-sync is handled by OpenXR
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	
+	else:
+		# couldn't start OpenXR.
+		print("OpenXR not instantiated!")
+		get_tree().quit()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var x = randi()
+	var arr: Array = [x,delta]
