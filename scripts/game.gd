2127c40f7a1ce4b60ed7cf65ced2d426c6356970
Albert Khayat <1albertkhayat1@gmail.com>
2024-05-14 18:27:38 +0300

    Add a scene for first micro game

---
+++ scripts/game.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+# Path to song that game is playing
+@export_dir var song_path: String
+
+@onready var player = $Player
+@onready var cannon = $Cannon
+@onready var audio_player = $AudioStreamPlayer
+
+var song: Song
+var interface: XRInterface
+
+func _init_vr():
+	interface = XRServer.find_interface("OpenXR")
+	if interface and interface.is_initialized():
+		print("OpenXR instantiated successfully.")
+		var vp : Viewport = get_viewport()
+		
+		# Enable xr on viewport
+		vp.use_xr = true
+		
+		# Make sure v-sync is off, v-sync is handled by OpenXR
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	
+	else:
+		# couldn't start OpenXR.
+		print("OpenXR not instantiated!")
+		get_tree().quit()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	song = Song.new(song_path)
+	audio_player.stream = load(song.audio_path)
+	_init_vr()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
