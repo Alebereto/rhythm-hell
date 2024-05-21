709c12c3b42cb2268f4924df54a2fd4f3fbbfa7d
Albert Khayat <1albertkhayat1@gmail.com>
2024-05-21 21:04:50 +0300

    Connect some signals to test cannon

--- scenes/karate/karate_game.gd
+++ scenes/karate/karate_game.gd
@@ -1,9 +1,8 @@
 extends GameController
 
 @export var cannon: Node3D
-@export var player: Player
 
-
+signal fire_cannon
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,3 +12,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func connect_signals():
+	var right_controller: XRController3D = player.find_child("RightController")
+	var left_controller: XRController3D = player.find_child("LeftController")
+
+	right_controller.button_pressed.connect(_on_right_button_press)
+
+func _input(event):
+	if event.is_action_pressed("test-shoot"):
+		print("Shooting!!")
+		_shoot_cannon()
+
+func _shoot_cannon():
+	fire_cannon.emit("res://scenes/karate/assets/projectiles/projectile.tscn", 1)
+
+func _on_right_button_press(button: String):
+	print("Pressed %s" %button)
+	_shoot_cannon()
