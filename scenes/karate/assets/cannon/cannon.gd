1cc131aa152b68eb05e3107b2cbb793da926d421
Albert Khayat <1albertkhayat1@gmail.com>
2024-05-21 21:00:29 +0300

    Add ability to shoot cannon with given time by calculating impulse vector

--- scenes/karate/assets/cannon/cannon.gd
+++ scenes/karate/assets/cannon/cannon.gd
@@ -1,39 +1,69 @@
 extends Node3D
 
-@export var target: Node3D
 
+@export var target: Node3D # Cannon target node
+@export var shooter: Node3D # Object that calls "fire_cannon" signal
+
+
+@export var projectiles_root: Node3D
+
+@onready var fire_cannon: Signal = shooter.fire_cannon # shooter's fire cannon signal
 @onready var anchor: Node3D = $Cylinder/ShootAnchor
-@onready var projectiles_root: Node3D = $Projectiles
 
+# Source and target vectors of cannon fire
 var source_pos: Vector3: get = _get_source_pos
 var target_pos: Vector3: get = _get_target_pos
 
+# Gravity parameters from settings
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var gravity_vector: Vector3 = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 
-func _get_source_pos(): return anchor.position
-func _get_target_pos(): return target.position
 
-# get 
-func _calculate_impulse(time: float, mass: float) -> Vector3:
-	var delta_pos = target_pos - source_pos
-	return Vector3(1,1,1) * time
+# getters for source and target vectors
+func _get_source_pos(): return anchor.global_position
+func _get_target_pos(): return target.global_position
 
-func shoot(projectile_scene: String, time: float) -> void:
-	var projectile: RigidBody3D = load(projectile_scene).instantiate()
-	projectile.position = anchor.position
-	projectiles_root.add_child(projectile)
-	
-	var impulse = _calculate_impulse(time, projectile.mass)
-	projectile.apply_impulse(impulse)
-	# ==== maybe add as child???
-
-
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	# connect fire signal from shooter to cannon
+	fire_cannon.connect(fire)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# func _process(delta):
+# 	pass
+	
+
+## Get impulse vector for projectile given travel time and projectile mass
+##
+func _calculate_impulse(time: float, projectile: RigidBody3D) -> Vector3:
+
+	# get position of projectile source
+	var proj_pos = projectile.global_position
+
+	var impulse: Vector3 = (target_pos - proj_pos) / time
+	impulse.y += (0.5 * gravity * time)
+
+	impulse /= projectile.mass
+
+	return impulse
+
+func _create_projectile(projectile_scene: String, start_pos: Vector3) -> RigidBody3D:
+	# Instantiate projectile
+	var projectile: RigidBody3D = load(projectile_scene).instantiate()
+	# Set initial position
+	projectile.global_position = start_pos
+	# Add projectile to scene
+	projectiles_root.add_child(projectile)
+
+	return projectile
+
+## Gets projectile scene path and time for projectile to reach its destination,
+## instantiates projectile and fires it
+##
+func fire(projectile_scene: String, time: float) -> void:
+	# Make projectile
+	var projectile: RigidBody3D = _create_projectile(projectile_scene, source_pos)
+	
+	# Give impulse to projectile
+	var impulse = _calculate_impulse(time, projectile)
+	projectile.apply_impulse(impulse)
