0c6b8c16b5817ca4d2928944736b1ec7d264b60d
Albert Khayat <1albertkhayat1@gmail.com>
2024-05-31 01:37:21 +0300

    Add functionality for detecting area

---
+++ scenes/level_editor/assets/level_editor_menu/items/note_2d.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+const BAR_GAP: float = 64.0
+
+var _note_info: Globals.NoteInfo
+
+
+# Get refrences to note rects
+@onready var _note_rect: ColorRect = $NoteRect
+@onready var _start_rect: ColorRect = $StartRect
+@onready var _connector: ColorRect = $Connector
+
+@onready var _area: Area2D = $Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_note_info = Globals.NoteInfo.new()
+	update_visuals()
+
+func copy_other(note: Node2D) -> void:
+	set_data(note.get_data())
+	update_visuals()
+
+
+
+func get_data() -> Globals.NoteInfo:
+	return _note_info.clone()
+
+func set_data(data: Globals.NoteInfo) -> void:
+	_note_info = data
+
+
+func set_pickable() -> void:
+	_area.collision_layer = 1
+
+
+# set visuals functions
+
+func update_visuals() -> void:
+	_set_colors(_note_info.color)
+	_set_sizes(BAR_GAP)
+
+func _set_colors(c: Color) -> void:
+	# set note rect color
+	_note_rect.color = c
+	# set start rect color
+	_start_rect.color = c
+
+	# set _connector color
+	var connector_color = c
+	connector_color.a = 0.5
+	_connector.color = connector_color
+
+func _set_sizes(bar_gap: float) -> void:
+	if _note_info.rotated: _note_rect.rotation = deg_to_rad(45.0)
+	else:				   _note_rect.rotation = 0.0
+
+	var start_pos = -(_note_info.delay) * bar_gap
+
+	_start_rect.position.x = start_pos
+	_connector.position.x = start_pos
+	_connector.size.x = -start_pos
