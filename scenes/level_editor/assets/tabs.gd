531816e792514c1bf0261817f2fe160f1433acd5
Albert Khayat <1albertkhayat1@gmail.com>
2024-05-31 01:47:50 +0300

    Internalize tabs inputs

--- scenes/level_editor/assets/tabs.gd
+++
@@ -1,8 +0,0 @@
-extends HBoxContainer
-
-@onready var file_tab: MenuButton = $File
-@onready var view_tab: MenuButton = $View
-
-var file_popup: PopupMenu: get = _get_file_popup
-
-func _get_file_popup() -> PopupMenu: return file_tab.get_popup()
