f65195c5b8a454e5626a401662dcc12df9274899
Albert Khayat <1albertkhayat1@gmail.com>
2024-05-31 04:44:04 +0300

    Make minor improvements

--- Objects/player.gd
+++ Objects/player.gd
@@ -1,27 +1,14 @@
 class_name Player extends Node3D
 
-var interface: XRInterface
-@onready var button_pressed: Signal = $RightController.button_pressed
+
+signal trigger_pressed
+signal right_hand_entered_body(body)
 
 
-func _init_vr():
-	interface = XRServer.find_interface("OpenXR")
-	if interface and interface.is_initialized():
-		print("OpenXR instantiated successfully.")
-		var vp : Viewport = get_viewport()
-		
-		# Enable xr on viewport
-		vp.use_xr = true
-		
-		# Make sure v-sync is off, v-sync is handled by OpenXR
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-	
-	else:
-		# couldn't start OpenXR.
-		print("OpenXR not instantiated!")
-		# get_tree().quit()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	_init_vr()
+func _on_right_hand_body_entered(body):
+	right_hand_entered_body.emit(body)
 
+
+func _on_right_controller_button_pressed(button_name):
+	if button_name == "trigger_click": trigger_pressed.emit()
--- scenes/karate/karate_game.gd
+++ scenes/karate/karate_game.gd
@@ -6,8 +6,8 @@ extends GameController
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	note_started.connect(_play_note)
 	super()
+	note_started.connect(_play_note)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +18,12 @@ func _play_note(note: Dictionary):
 	var fire_time = note["b"] - note["s"]
 	_cannon.fire(_projectile_path, fire_time)
 
+
+
+func _on_player_right_hand_entered_body(body):
+	if not (body is RigidBody3D): return
+	body.apply_impulse(Vector3(0,0,-10))
+
+
+func _on_player_trigger_pressed():
+	_start_song()
