e1ed0f8a172eda685504c0f8366e937ef4f43d6e
Albert Khayat <1albertkhayat1@gmail.com>
2024-07-28 20:20:09 +0300

    Add script for the vr viewport

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,33 +4,16 @@ extends Node
 const MainMenuScene: PackedScene = preload("res://scenes/main_menu/main_menu.tscn")
 const GameControllerScene: PackedScene = preload("res://scenes/game_controller/game_controller.tscn")
 
-var _xr_interface: XRInterface
 
 var _current_scene: Node = null	# The current scene that player is in
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# _init_vr()
 	
 	# await _load_main_menu()
 	await _load_game_controller()	# temp line
 
-
-func _init_vr():
-	_xr_interface = XRServer.find_interface("OpenXR")
-	if _xr_interface and _xr_interface.is_initialized():
-		print("OpenXR instantiated successfully.")
-
-		# Make sure v-sync is off, v-sync is handled by OpenXR
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-		
-		# Enable xr on viewport
-		get_viewport().use_xr = true
-	
-	else:
-		# couldn't start OpenXR.
-		print("OpenXR not instantiated!")
 
 
 func _load_main_menu():
