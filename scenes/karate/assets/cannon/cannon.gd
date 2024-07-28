a736c1f438dcc571b95506ccd1304d5a567fcd87
Albert Khayat <1albertkhayat1@gmail.com>
2024-07-28 20:19:32 +0300

    Add some visual effects to the cannon

--- scenes/karate/assets/cannon/cannon.gd
+++ scenes/karate/assets/cannon/cannon.gd
@@ -1,9 +1,15 @@
 extends Node3D
 
+const SHOOT_ANIMATION_DURATION: float = 0.8
+const SHOOT_ANIMATION_DELAY: float = 0.3
+const SHOOT_ANIMATION_NAME: String = "cannon_shoot"
+
 
 @export var _target: Node3D # Cannon target node
 
-@onready var _anchor: Node3D = $Cylinder/ShootAnchor
+@onready var _anchor: Node3D = $ShootAnchor
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+@onready var _poof_effect: GPUParticles3D = $PoofEffect
 
 # Source and _target vectors of cannon fire
 var _source_pos: Vector3: get = _get_source_pos
@@ -17,6 +23,11 @@ var _gravity_vector: Vector3 = ProjectSettings.get_setting("physics/3d/default_g
 # getters for source and _target vectors
 func _get_source_pos(): return _anchor.global_position
 func _get_target_pos(): return _target.global_position
+
+
+func _play_shoot_animation():
+	if _animation_player.is_playing(): _animation_player.stop()
+	_animation_player.play(SHOOT_ANIMATION_NAME)
 	
 
 ## Get impulse vector for given projectile for reaching _source_pos
@@ -43,3 +54,5 @@ func fire(projectile: Projectile, travel_time: float) -> void:
 	# Give impulse to projectile
 	var impulse = _calculate_impulse(projectile, travel_time)
 	projectile.apply_impulse(impulse)
+	# Add poof effect
+	_poof_effect.emitting = true
